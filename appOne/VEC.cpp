46baf0aa46bcb93a7ce21ac1653b17814105bbd6
謙心 田中 <Nerei0514@outlook.jp>
2021-06-11 10:58:16 +0900

    プロジェクト ファイルを追加します。

---
+++ appOne/VEC.cpp
@@ -0,0 +1,25 @@
+#include"libOne.h"
+#include"VEC.h"
+float magnitude(struct VEC* v) {
+	return sqrt(v->x * v->x + v->y * v->y);
+}
+struct VEC normalize(struct VEC* v) {
+	float mag = magnitude(v);
+	struct VEC tmp;
+	if (mag > 0) {
+		tmp.x = v->x / mag;
+		tmp.y = v->y / mag;
+	}
+	return tmp;
+}
+struct VEC vecFromAngle(float angle) {
+	struct VEC tmp;
+	tmp.x = sin(angle);
+	tmp.y = -cos(angle);
+	return tmp;
+}
+float angleBetweenTwoVecs(struct VEC* a, struct VEC* b) {
+	float dp = a->x * b->x + a->y * b->y;
+	float cp = a->x * b->y - a->y * b->x;
+	return atan2(cp, dp);
+}
